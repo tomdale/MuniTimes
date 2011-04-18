89175b5e592cc8084fa195bef3f45da2ef0549b1
Tom Dale <tom@sproutcore.com>
2011-04-18 10:43:21 -0700

    Update UI to be iPhone friendly. Add iScroll4.

--- js/muni.coffee
+++ js/muni.coffee
@@ -1,31 +1,59 @@
 # Define the application namespace
 window.Muni = Muni = SC.Object.create
 
-  # Define an array controller that manages the available Muni routes
-  routesController: SC.ArrayController.create()
+  Route: SC.Object.extend
+    tag: null,
+    title: null
 
-  routesView: SC.TemplateCollectionView.extend
-    contentBinding: 'Muni.routesController'
+  API: SC.Object.create
+    loadRoutes: ->
+      # Fetch the XML feed of currently available routes for Muni
+      response = jQuery.ajax "/_strobe/proxy/webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=sf-muni"
+        success: (response) ->
+          routes = Muni.API._convertRoutesXMLToObjects(response)
+          Muni.routesController.set 'content', routes
 
-# Registers a function to run when SproutCore has finished loading.
-SC.ready ->
-  # Fetch the XML feed of currently available routes for Muni
-  response = jQuery.ajax "http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=sf-muni"
-    success: ->
+    loadStopsForRoute: (route) ->
+      tag = route.get('tag')
+      jQuery.ajax "/_strobe/proxy/webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=sf-muni&r=#{tag}"
+
+
+    _convertRoutesXMLToObjects: (xml) ->
       # Get a jQuery object for the parsed XML
-      body = $(response.responseXML).find 'body > route'
+      body = $(xml).find 'body > route'
       # Create an array to hold the routes
       routes = []
 
       body.each ->
         route = $(this)
-        routes.push
+        routes.push Muni.Route.create
           tag:   route.attr('tag')
           title: route.attr('title')
 
-      Muni.routesController.set 'content', routes
+      routes
 
+
+
+  # Define an array controller that manages the available Muni routes
+  routesController: SC.ArrayController.create
+    contentDidChange: (->
+      window.scrollView.refresh()
+    ).observes 'content', 'content.[]'
+
+  RoutesView: SC.TemplateCollectionView.extend
+    itemViewClass: SC.TemplateView.extend
+      templateName: 'route-item'
+      tagName: 'li'
+
+
+
+
+# Registers a function to run when SproutCore has finished loading.
+SC.ready ->
+  Muni.API.loadRoutes()
   view = SC.TemplateView.create
     templateName: 'loading'
 
   view.append()
+  window.scrollView = new iScroll('wrapper');
+
--- js/muni.js
+++ js/muni.js
@@ -1,32 +1,60 @@
 (function() {
   var Muni;
   window.Muni = Muni = SC.Object.create({
-    routesController: SC.ArrayController.create(),
-    routesView: SC.TemplateCollectionView.extend({
-      contentBinding: 'Muni.routesController'
-    })
-  });
-  SC.ready(function() {
-    var response, view;
-    response = jQuery.ajax("http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=sf-muni", {
-      success: function() {
+    Route: SC.Object.extend({
+      tag: null,
+      title: null
+    }),
+    API: SC.Object.create({
+      loadRoutes: function() {
+        var response;
+        return response = jQuery.ajax("/_strobe/proxy/webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=sf-muni", {
+          success: function(response) {
+            var routes;
+            routes = Muni.API._convertRoutesXMLToObjects(response);
+            return Muni.routesController.set('content', routes);
+          }
+        });
+      },
+      loadStopsForRoute: function(route) {
+        var tag;
+        tag = route.get('tag');
+        return jQuery.ajax("/_strobe/proxy/webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=sf-muni&r=" + tag);
+      },
+      _convertRoutesXMLToObjects: function(xml) {
         var body, routes;
-        body = $(response.responseXML).find('body > route');
+        body = $(xml).find('body > route');
         routes = [];
         body.each(function() {
           var route;
           route = $(this);
-          return routes.push({
+          return routes.push(Muni.Route.create({
             tag: route.attr('tag'),
             title: route.attr('title')
-          });
+          }));
         });
-        return Muni.routesController.set('content', routes);
+        return routes;
       }
-    });
+    }),
+    routesController: SC.ArrayController.create({
+      contentDidChange: (function() {
+        return window.scrollView.refresh();
+      }).observes('content', 'content.[]')
+    }),
+    RoutesView: SC.TemplateCollectionView.extend({
+      itemViewClass: SC.TemplateView.extend({
+        templateName: 'route-item',
+        tagName: 'li'
+      })
+    })
+  });
+  SC.ready(function() {
+    var view;
+    Muni.API.loadRoutes();
     view = SC.TemplateView.create({
       templateName: 'loading'
     });
-    return view.append();
+    view.append();
+    return window.scrollView = new iScroll('wrapper');
   });
 }).call(this);
